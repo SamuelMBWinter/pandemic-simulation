e9844cf1c211059e558fb5fb4c04ce216ddf6ed6
Samuel M B Winter <Samuel.M.B.Winter@protonmail.com>
2021-04-10 12:06:44 +0000

    updating the spreading mechanism

--- simulation.py
+++ simulation.py
@@ -1,3 +1,7 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
 class Person:
     def __init__(self, infection_status='s'):
         self.infection_status = infection_status
@@ -7,11 +11,30 @@ class Person:
 
 class Polulation:
     def __init__(self, N):
-        self.people = [Person('s')] * N
-
-    def print_people(self):
+        self.people = [Person('s') for i in range(N)] # Generates a list of people
+        self.size = len(self.people)
+    
+   def print_people(self):
         print(self.people)
 
-    def 
+    def update_day(self):
+
+        for person in self.people:
+            if person.infection_status == 'i':
+                contacts = np.floor(self.size * random.random() ** 2)
+                print(contacts)
+                for p in random.sample(self.people, self.size):
+                    p.infection_status = 'i'
+            else:
+                pass
+    
+
 pop = Polulation(5)
+spawn = pop.people[0]
+spawn.infection_status = 'i'
 pop.print_people()
+
+pop.update_day()
+
+pop.print_people()
+
